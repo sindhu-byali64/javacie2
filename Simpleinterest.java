61ed1179ffb0d91fd52adeea9f18379fae5c8805
sindhu-byali64 <sindhutbyali6@gamil.com>
2025-04-09 15:59:44 +0530

    save

---
+++ Simpleinterest.java
@@ -0,0 +1,13 @@
+package cie2;
+
+public class Simpleinterest{
+        public static void main(String[] args) {
+            double principal = 1000;  // Principal amount
+            double rate = 5;          // Interest rate (in %)
+            double time = 2;          // Time (in years)
+    
+            double interest = (principal * rate * time) / 100;  // Simple Interest formula
+    
+            System.out.println("Simple Interest: " + interest);
+        }
+}
